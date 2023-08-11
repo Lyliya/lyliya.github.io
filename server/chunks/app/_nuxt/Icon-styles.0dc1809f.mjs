53fcaab8aca517f4e17c5e4624600ae2ef0049c8
Guillaume Guerin <guillaume.guerin@epitech.eu>
2023-08-11 13:49:36 +0200

    Updates

--- server/chunks/app/_nuxt/Icon-styles.0dc1809f.mjs
+++
@@ -1,6 +0,0 @@
-const Icon_vue_vue_type_style_index_0_scoped_c3ad5561_lang = ".icon[data-v-c3ad5561]{display:inline-block;vertical-align:middle}";
-
-const IconStyles_0dc1809f = [Icon_vue_vue_type_style_index_0_scoped_c3ad5561_lang];
-
-export { IconStyles_0dc1809f as default };
-//# sourceMappingURL=Icon-styles.0dc1809f.mjs.map
